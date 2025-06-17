367ec8eead6acf13e02e91715e75cc13e9c9f8a4
Thushan2002 <selvakumarthushanthan4@gmail.com>
2025-06-17 22:01:55 +0530

    feat: sign up page added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,7 +1,15 @@
 import React from "react";
+import { Routes, Route } from "react-router-dom";
+import Signup from "./pages/auth/Signup";
 
 const App = () => {
-  return <div>App</div>;
+  return (
+    <div>
+      <Routes>
+        <Route path="/signup" element={<Signup />} />
+      </Routes>
+    </div>
+  );
 };
 
 export default App;
