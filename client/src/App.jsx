3d59ff3ba53e2996cf61457e07fcc36e320e93db
Thushan2002 <selvakumarthushanthan4@gmail.com>
2025-06-17 22:21:29 +0530

    feat: Home page updated

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -2,11 +2,13 @@ import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Signup from "./pages/auth/Signup";
 import Login from "./pages/auth/Login";
+import HomePage from "./pages/home/HomePage";
 
 const App = () => {
   return (
     <div>
       <Routes>
+        <Route path="/" element={<HomePage />} />
         <Route path="/signup" element={<Signup />} />
         <Route path="/login" element={<Login />} />
       </Routes>
