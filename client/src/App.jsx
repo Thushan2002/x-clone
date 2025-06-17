945c79d929d06aaec03f9165a7c8c2718a8118a2
Thushan2002 <selvakumarthushanthan4@gmail.com>
2025-06-17 22:12:23 +0530

    feat: login page added

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,12 +1,14 @@
 import React from "react";
 import { Routes, Route } from "react-router-dom";
 import Signup from "./pages/auth/Signup";
+import Login from "./pages/auth/Login";
 
 const App = () => {
   return (
     <div>
       <Routes>
         <Route path="/signup" element={<Signup />} />
+        <Route path="/login" element={<Login />} />
       </Routes>
     </div>
   );
