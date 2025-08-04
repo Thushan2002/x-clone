6fc6ec75d1cbe99fe0753be80264bea013ec85ae
Thushan2002 <selvakumarthushanthan4@gmail.com>
2025-08-04 19:20:11 +0530

    feat: add post function added to client

--- client/src/pages/home/CreatePost.jsx
+++ client/src/pages/home/CreatePost.jsx
@@ -1,24 +1,58 @@
 import { CiImageOn } from "react-icons/ci";
 import { BsEmojiSmileFill } from "react-icons/bs";
-import { useRef, useState } from "react";
+import { use, useRef, useState } from "react";
 import { IoCloseSharp } from "react-icons/io5";
+import { useMutation, useQuery, useQueryClient } from "@tanstack/react-query";
+import LoadingSpinner from "../../components/common/LoadingSpinner";
+import baseUrl from "../../constatant/url";
+import toast from "react-hot-toast";
 
 const CreatePost = () => {
+  const { data: authUser } = useQuery({ queryKey: ["authUser"] });
+  const queryClient = useQueryClient();
+  const {
+    mutate: createPost,
+    isError,
+    error,
+    isPending,
+  } = useMutation({
+    mutationFn: async ({ text, img }) => {
+      const res = await fetch(`${baseUrl}/api/post/create`, {
+        method: "POST",
+        credentials: "include",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({ text, img }),
+      });
+
+      const data = await res.json();
+
+      if (!res.ok || data.error) {
+        throw new Error(data.error || "Failed to delete post");
+      }
+      return data;
+    },
+    onSuccess: () => {
+      toast.success("Post Created successfully");
+      queryClient.invalidateQueries({ queryKey: ["posts"] });
+      setImg(null);
+      setText("");
+    },
+    onError: (error) => {
+      console.error("Post Creation failed:", error.message);
+      toast.error(error.message || "Failed to delete post");
+    },
+  });
+
   const [text, setText] = useState("");
   const [img, setImg] = useState(null);
 
   const imgRef = useRef(null);
 
-  const isPending = false;
-  const isError = false;
-
-  const data = {
-    profileImg: "/avatars/boy1.png",
-  };
-
   const handleSubmit = (e) => {
     e.preventDefault();
-    alert("Post created successfully");
+    createPost({ text, img });
   };
 
   const handleImgChange = (e) => {
@@ -36,7 +70,7 @@ const CreatePost = () => {
     <div className="flex p-4 items-start gap-4 border-b border-gray-700">
       <div className="avatar">
         <div className="w-8 rounded-full">
-          <img src={data.profileImg || "/avatar-placeholder.png"} />
+          <img src={authUser.profileImg || "/avatar-placeholder.png"} />
         </div>
       </div>
       <form className="flex flex-col gap-2 w-full" onSubmit={handleSubmit}>
@@ -78,10 +112,10 @@ const CreatePost = () => {
             onChange={handleImgChange}
           />
           <button className="btn btn-primary rounded-full btn-sm text-white px-4">
-            {isPending ? "Posting..." : "Post"}
+            {isPending ? <LoadingSpinner /> : "Post"}
           </button>
         </div>
-        {isError && <div className="text-red-500">Something went wrong</div>}
+        {isError && <div className="text-red-500">{error.message}</div>}
       </form>
     </div>
   );
